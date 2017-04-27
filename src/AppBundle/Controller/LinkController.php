3af61b18522b5f91e28c3c95bb4f89af7703f8b8
STAGIAIRE <STAGIAIRE@STA6018729.exchange.ad.afpanet>
2017-04-27 11:12:34 +0200

    save link

---
+++ src/AppBundle/Controller/LinkController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\Link;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Embed\Embed;
+
+
+class LinkController extends Controller
+{
+  /**
+   * @Route("/recup-url", name="recupurlpage")
+   */
+  public function recupUrlAction(Request $request)
+  {
+    //create an entity to use with form
+    $link = new Link();
+    // create Form builder
+    $formBuilder = $this->createFormBuilder($link);
+    //add fields
+    $formBuilder->add('url', UrlType::class);
+    $formBuilder->add('enregistrer', SubmitType::class);
+    // get the formBuilder
+    $form = $formBuilder->getForm();
+
+    $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+             // $form->getData() holds the submitted values
+             // but, the original `$task` variable has also been updated
+             $link = $form->getData();
+             //Create the "embed" of the link
+             $info = Embed::create($link->getUrl());
+             //get the info of the URL
+             $title = $info->title;
+             $description = $info->description;
+             $type = $info->type;
+             $image = $info->image;
+             $format = 'd-m-Y';
+             $publishedDate = date("d-m-Y");
+             $publishedTime = \DateTime::createFromFormat($format, $publishedDate);
+             //set
+             $link->setTitle($title);
+             $link->setDescription($description);
+             $link->setType($type);
+             $link->setImage($image);
+             $link->setPublishedDate($publishedTime);
+             //Save the URL to BDD
+             $em = $this->getDoctrine()->getManager();
+             $em->persist($link);
+             $em->flush();
+
+             return $this->render('link/resultat.html.twig',
+             [
+                  'url' => $link->getURL(),
+                  'image' => $link->getImage(),
+                  'description' => $link->getDescription(),
+                  'title' => $link->getTitle(),
+              ]);
+          }
+
+      return $this->render('link/recup-url.html.twig',
+      [
+          'form' => $form->createView(),
+      ]);
+  }
+
+}
---
+++ src/AppBundle/Entity/Link.php
@@ -0,0 +1,209 @@
+<?php
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="link")
+ */
+class Link
+{
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $title;
+
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    private $description;
+
+
+    /**
+     * @ORM\Column(type="string", length=100, nullable=true)
+     */
+    private $type;
+
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    private $image;
+
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $publishedDate;
+
+
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     *
+     * @return Link
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Link
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Link
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return Link
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return Link
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set publishedDate
+     *
+     * @param \DateTime $publishedDate
+     *
+     * @return Link
+     */
+    public function setPublishedDate($publishedDate)
+    {
+        $this->publishedDate = $publishedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get publishedDate
+     *
+     * @return \DateTime
+     */
+    public function getPublishedDate()
+    {
+        return $this->publishedDate;
+    }
+}
