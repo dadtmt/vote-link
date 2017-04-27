7b4e38604b8288714c1381ebf9b91ae06e0410f9
Thomas Culdaut <thomas.culdaut@gmail.com>
2017-04-27 09:15:15 +0200

    change toto

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,8 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => 'toto',
+         return $this->render('default/index.html.twig', [
+            'base_dir' => 'vote link app',
         ]);
     }
 }
