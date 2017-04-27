189ca8da681d94b1c69e8bc684784c7ba246742c
STAGIAIRE <STAGIAIRE>
2017-04-27 10:16:56 +0200

    ImageSave

---
+++ src/AppBundle/ImageSave/ImageSave.php
@@ -0,0 +1,34 @@
+<?php
+namespace AppBundle\ImageSave;
+
+use Doctrine\ORM\Mapping as ORM;
+use Imagine\Image\Box;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
+
+class ImageSave
+{
+    public static function save($url)
+    {
+        try {
+            // Generate a version 5 (name-based and hashed with SHA1) UUID object
+            $uuid5 = Uuid::uuid5(Uuid::NAMESPACE_DNS, $url);
+            $filename = $uuid5->toString();
+            $ext = pathinfo($url, PATHINFO_EXTENSION);
+            $content = file_get_contents($url);
+            $fp = fopen("../var/image/".$filename.".".$ext , "w");
+            fwrite($fp, $content);
+            fclose($fp);
+
+            $imagine = new \Imagine\Gd\Imagine();
+            $image = $imagine->open("../var/image/".$filename.".".$ext);
+            $image->resize(new Box(200, 200))->save("../web/image/".$filename.".".$ext);
+
+            return 'image/'.$filename.'.'.$ext;
+        } catch (\Exception $e) {
+            // Some dependency was not met. Either the method cannot be called on a
+            // 32-bit system, or it can, but it relies on Moontoast\Math to be present.
+            return null;
+        }
+    }
+}
